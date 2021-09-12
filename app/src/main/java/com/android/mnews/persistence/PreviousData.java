0a247606a12d5ac3891755d72afa164a40b645e3
MVA27 <manvekar906@gmail.com>
2021-09-12 19:40:49 +0530

    PreviousData.java added : It saves/loads json from SharedPreferences file

---
+++ app/src/main/java/com/android/mnews/persistence/PreviousData.java
@@ -0,0 +1,26 @@
+package com.android.mnews.persistence;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreviousData {
+
+    private Context context;
+    private final String PREVIOUS_DATA = "PREVIOUS_DATA";
+
+    public PreviousData(Context context) {
+        this.context = context;
+    }
+
+    public void saveData(String json){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREVIOUS_DATA,Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(PREVIOUS_DATA,json);
+        editor.apply();
+    }
+
+    public String getData(){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREVIOUS_DATA,Context.MODE_PRIVATE);
+        return sharedPreferences.getString(PREVIOUS_DATA,""); //TODO : Put default values
+    }
+}
