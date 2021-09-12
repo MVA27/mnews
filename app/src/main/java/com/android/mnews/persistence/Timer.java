cf48dc24ef1a095ae6806a16a048d0bb789c1dd8
MVA27 <manvekar906@gmail.com>
2021-09-12 20:08:25 +0530

    Timer.java added : It saves/loads time at which user entered the application last time from/into SharedPreferences file. This helps in reducing API calls

---
+++ app/src/main/java/com/android/mnews/persistence/Timer.java
@@ -0,0 +1,66 @@
+package com.android.mnews.persistence;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Timer {
+
+    private Context context;
+    private SharedPreferences sharedPreferences;
+    private final String TIMER_FILE = "TIMER_FILE";
+    private final String HOURS = "HOURS";
+    private final String MINUTES = "MINUTES";
+
+
+    Timer(Context context){
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences(TIMER_FILE,Context.MODE_PRIVATE);
+    }
+
+    public void saveCurrentEnteringTime(){
+        String hrs = new SimpleDateFormat("HH", Locale.getDefault()).format(new Date());
+        String min = new SimpleDateFormat("mm",Locale.getDefault()).format(new Date());
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(HOURS,hrs);
+        editor.putString(MINUTES,min);
+        editor.apply();
+    }
+
+    public String[] getPreviouslyEnteredTime(){
+        String hrs = sharedPreferences.getString(HOURS,"0");
+        String min = sharedPreferences.getString(MINUTES,"0");
+        return new String[]{hrs,min};
+    }
+
+    public int getDuration(){
+
+        //Get The time user entered last time
+        String[] s = getPreviouslyEnteredTime();
+        String oldHrs = s[0];
+        String oldMin = s[1];
+
+        //Calculate Difference
+        String currentHrs = new SimpleDateFormat("HH",Locale.getDefault()).format(new Date());
+        String currentMin = new SimpleDateFormat("mm",Locale.getDefault()).format(new Date());
+
+        int differenceInHours = Integer.parseInt(oldHrs) - Integer.parseInt(currentHrs);
+        int differenceInMinutes = 0;
+        if(Math.abs(differenceInHours) == 0){
+            differenceInMinutes =  Integer.parseInt(oldMin) - Integer.parseInt(currentMin);
+        }
+        else{
+            differenceInMinutes = -1;
+        }
+
+        //Save Current Time
+        saveCurrentEnteringTime();
+
+        return differenceInMinutes;
+    }
+
+}
