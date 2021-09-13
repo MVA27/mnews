167a28b5fa6349b8803157e8d894c8457445cb14
MVA27 <manvekar906@gmail.com>
2021-09-13 23:03:01 +0530

    Few Enhancement

--- app/src/main/java/com/android/mnews/MainActivity.java
+++ app/src/main/java/com/android/mnews/MainActivity.java
@@ -2,7 +2,6 @@ package com.android.mnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,6 +9,7 @@ import android.widget.Toast;
 import com.android.mnews.administrator.ThreadParseHTML;
 import com.android.mnews.mediastack.Data;
 import com.android.mnews.mediastack.Holder;
+import com.android.mnews.persistence.Timer;
 import com.android.mnews.threads.ThreadDataLoader;
 import java.util.List;
 
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     public static Holder holder;
     public static List<Data> data;
-    Button goButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,21 +36,12 @@ public class MainActivity extends AppCompatActivity {
             if (MainActivity.data == null) {
                 new ThreadDataLoader(ThreadParseHTML.holder.getKey()).start();
             }
-
             //If data is not loaded, loop until it does
-            while (MainActivity.data == null) ;
+            while (MainActivity.data == null);
 
             //Move to Display activity
             Intent intent = new Intent(MainActivity.this, ActivityDisplay.class);
             startActivity(intent);
-
-            goButton = findViewById(R.id.Go_button);
-            goButton.setOnClickListener(v -> {
-                Toast.makeText(MainActivity.this, "data size = " + data.size(), Toast.LENGTH_SHORT).show();
-                Intent intent1 = new Intent(MainActivity.this, ActivityDisplay.class);
-                startActivity(intent1);
-            });
-
         }
     }
 
